9ac61eaa19bb83f3e25c1d73a68bd2f31ca5c69d
Jakub Wawruszczak <jakubwawruszczak2@gmail.com>
2026-03-12 17:17:41 +0100

    stworzyłęm strukture krzemu (parametry do uzupełnienia)

--- Transport/Transport/main.cpp
+++ Transport/Transport/main.cpp
@@ -9,8 +9,22 @@
 #include "distribution.h"
 
 
+struct Si
+{
+	double meff_l = 1;
+	double meff_t = 1;
+	double tau_op = 1;
+	double tau_ac = 1;
+	double tau_ion = 1;
+	double N_d = 1;
+
+	Si(double meff_l, double meff_t, double tau_op, double tau_ac, double tau_ion, double N_d)
+		: meff_l(meff_l), meff_t(meff_t), tau_op(tau_op), tau_ac(tau_ac), tau_ion(tau_ion), N_d(N_d) {}
+};
+
+
 int main()
 {
-	int a = 1;
+	return 0;
 }
 
