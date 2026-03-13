cb69a94fc57bb5254c2d9b51555c9e8d1534779b
Jagoda Malag <jagoda2305@gmail.com>
2026-03-13 12:03:46 +0100

    dodałam bandstruct.cpp ale trzeba jeszcze h zdefiniować

--- Transport/Transport/bandstruct.cpp
+++ Transport/Transport/bandstruct.cpp
@@ -1,8 +1,8 @@
 #include "bandstruct.h"
 #include "class.h"
 
-double Ek(double k, material Mat) // parabola na początek 
+double Ek(vec3d k, material Mat) // parabola na początek, h jeszcze podać 
 {
-	Ek = 
-	return 0.0;
+	Ek = h*h((k.x)*(k.x)/(2*meff_l)+(k.y)(k.y)/(2*meff_l)+(k.z)(k.z)/(2*meff_t)
+	return Ek;
 }
