c0c0fa62717a808c239c0576e6399e8b64327aa7
Jagoda Malag <jagoda2305@gmail.com>
2026-03-13 12:40:04 +0100

    bandstruct update i definicja delta_k w class

--- Transport/Transport/bandstruct.cpp
+++ Transport/Transport/bandstruct.cpp
@@ -3,6 +3,17 @@
 
 double Ek(vec3d k, material Mat) // parabola na początek, h jeszcze podać 
 {
-	Ek = h*h((k.x)*(k.x)/(2*meff_l)+(k.y)(k.y)/(2*meff_l)+(k.z)(k.z)/(2*meff_t)
+	double Ek = hbar*hbar*((k.x)*(k.x)/(2*Mat.meff_l)+(k.y)*(k.y)/(2*Mat.meff_l)+(k.z)*(k.z)/(2*Mat.meff_t));
 	return Ek;
+}
+
+double gradEk_anal(vec3d k, material Mat)
+{
+	double gradEk = hbar*hbar*((k.x)/(Mat.meff_l)+(k.y)/(Mat.meff_l)+(k.z)/(Mat.meff_t));
+	return gradEk;
+}
+
+double gradEk_Ek_num(vec3d k, material Mat, double delta_k)
+{
+	double gradEk = 
 }
