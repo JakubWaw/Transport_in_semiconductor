48da8b848206e5e7cc121b6fa86947195f487f68
Jagoda Malag <jagoda2305@gmail.com>
2026-03-19 16:39:55 +0100

    dobre dane dla Si

--- Transport/Transport/main.cpp
+++ Transport/Transport/main.cpp
@@ -14,7 +14,7 @@
 int main()
 {
 
-	material Si = material(0.98*m0, 0.98*m0, 0.19*m0, 10e-12, 10e-20, 10e-14, 10e-15);
+	material Si = material(0.98*m0, 0.98*m0, 0.19*m0, pow(10,-13.5), pow(10,-12.5), pow(10,-10), 1.2*10e16);
 	double Ek_Si = Ek(vec3d(10e10, 10e10, 10e10), Si);
 
 	std::cout<< Ek_Si;
